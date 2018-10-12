b785a2b8eb82eca266608bcdbdd42f907b6c7ff3
castle <dsalazar10@horizon.csueastbay.edu>
2018-10-12 11:13:43 -0700

    Adding new Feature: Print Function This simplifies the code for printing (skip "System.out").

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Simulate an edit from another team member...");
+    }
+    private static void Print(Stage stage, String s) {
+        Stage UIViewStage = null;
+        Group UIViewRoot;
+        Scene UIViewScene;
+
+        // Set the Scene
+        UIViewRoot = new Group();
+        UIViewScene = new Scene(UIViewRoot);
+
+        UIViewStage.setScene(UIViewScene);
+        UIViewStage = stage;
+        UIViewStage.setTitle("Canvas Example");
+        Add_Text("Hello World!", 10, 40);
+        UIViewStage.show();
+    }
+    private static void Add_Text(String msg, int l, int w) {
+        // l = 10, w = 40, msg = "Hello World!"
+        Text text = new Text(l, w, msg);
+        text.setFont(new Font(w));
+        Scene scene = new Scene(new Group(text));
     }
 }
